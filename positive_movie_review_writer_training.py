b1e47574a302d1f995fe4aa3d0236dee31062bfb
moosenasser <moustaphanasser@gmail.com>
2021-05-12 17:05:54 -0400

    Made reviews readable and cleaned up code.

--- positive_movie_review_writer_training.py
+++ positive_movie_review_writer_training.py
@@ -7,47 +7,48 @@ import tensorflow_datasets as tfds
 
 POSITIVE_VAL = 1
 
+BATCH_SIZE = 64
+EMBEDDING_DIM = 256
+RNN_UNITS = 1024
+
+""" Buffer size to shuffle dataset.
+(TensorFlow data is designed to work with potentially infinite sequences.
+So it does not try to shuffle entire sequence in memory.
+Rather, it maintains buffer in which it shuffles elements.) """
+BUFFER_SIZE = 10000
+
 training_data = tfds.load(name="imdb_reviews", split="train", batch_size=-1, as_supervised=True)
 
 training_data, training_labels = tfds.as_numpy(training_data)
-
 training_df = pd.DataFrame({'review-text': training_data, 'sentiment-value': training_labels})
-
-training_data_by_sentiment_val = training_df[training_df['sentiment-value'] == POSITIVE_VAL]
-review_text_by_sentiment_val = training_data_by_sentiment_val['review-text']
+training_data_by_positive_val = training_df[training_df['sentiment-value'] == POSITIVE_VAL]
+review_text_by_positive_val = training_data_by_positive_val['review-text']
 
 review_text_by_sentiment_str = ''
-review_text_by_sentiment_len = len(review_text_by_sentiment_val)
 
-# Access elements of Series converted to array to avoid error message.
-review_text_by_sentiment_arr = review_text_by_sentiment_val.values
+for i in range(len(review_text_by_positive_val)):
+    # Access elements of Series converted to array to avoid error message.
+    review_text_by_sentiment_str += review_text_by_positive_val.array[i].decode(encoding='utf-8')
 
-for i in range(review_text_by_sentiment_len):
-    review_text_by_sentiment_element_str = review_text_by_sentiment_arr[i].decode()
-
-    # Literal line break tags have to be removed from string.
-    review_text_by_sentiment_str += review_text_by_sentiment_element_str.replace('<br /><br />', '\n')
-
-    if i < (review_text_by_sentiment_len - 1):
-        review_text_by_sentiment_str += '\n\n'
+# Call ascii to prevent non-ascii characters from being included in output.
+review_text_by_sentiment_str = ascii(review_text_by_sentiment_str)
 
 vocab = sorted(set(review_text_by_sentiment_str))
-char2idx = {u: i for i, u in enumerate(vocab)}
-idx2char = np.array(vocab)
+char_to_idx = {u: i for i, u in enumerate(vocab)}
+vocab_arr = np.array(vocab)
 
 
 def text_to_int(text):
-    return np.array([char2idx[c] for c in text])
+    return np.array([char_to_idx[c] for c in text])
 
 
 text_as_int = text_to_int(review_text_by_sentiment_str)
 
 # Creating training examples.
 
-seq_length = 100  # Length of sequence for training example.
-examples_per_epoch = len(review_text_by_sentiment_str)//(seq_length + 1)
+seq_len = 100  # Length of sequence for training example.
 char_dataset = tf.data.Dataset.from_tensor_slices(text_as_int)
-sequences = char_dataset.batch(seq_length + 1, drop_remainder=True)
+sequences = char_dataset.batch(seq_len + 1, drop_remainder=True)
 
 
 def split_input_target(chunk):
@@ -58,12 +59,7 @@ def split_input_target(chunk):
 
 dataset = sequences.map(split_input_target)
 
-BATCH_SIZE = 64
 VOCAB_SIZE = len(vocab)
-EMBEDDING_DIM = 256
-RNN_UNITS = 1024
-
-BUFFER_SIZE = 10000
 
 data = dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE, drop_remainder=True)
 
@@ -82,7 +78,6 @@ def build_model(vocab_size, embedding_dim, rnn_units, batch_size):
 
 
 model = build_model(VOCAB_SIZE, EMBEDDING_DIM, RNN_UNITS, BATCH_SIZE)
-model.summary()
 
 
 def loss(labels, logits):
@@ -103,18 +98,17 @@ checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
     save_weights_only=True
 )
 
-history = model.fit(data, epochs=40, callbacks=[checkpoint_callback])
-
+model.fit(data, epochs=40, callbacks=[checkpoint_callback])
 model = build_model(VOCAB_SIZE, EMBEDDING_DIM, RNN_UNITS, batch_size=1)
 
 model.load_weights(tf.train.latest_checkpoint(checkpoint_dir))
 model.build(tf.TensorShape([1, None]))
 
 
-def generate_text(model_arg, start_string):
-    num_generate = 800
+def generate_text(model_arg, start_str):
+    num_chars_to_generate = 800
 
-    input_eval = [char2idx[s] for s in start_string]
+    input_eval = [char_to_idx[s] for s in start_str]
     input_eval = tf.expand_dims(input_eval, 0)
 
     text_generated = []
@@ -124,21 +118,21 @@ def generate_text(model_arg, start_string):
     # Batch size = 1 here.
     model_arg.reset_states()
 
-    for iteration in range(num_generate):
+    for iteration in range(num_chars_to_generate):
         predictions = model_arg(input_eval)
         predictions = tf.squeeze(predictions, 0)
-
         predictions = predictions / temperature
+
         predicted_id = tf.random.categorical(predictions, num_samples=1)[-1, 0].numpy()
 
         """ Pass predicted character as next input to model
         along with previous hidden state. """
         input_eval = tf.expand_dims([predicted_id], 0)
 
-        text_generated.append(idx2char[predicted_id])
+        text_generated.append(vocab_arr[predicted_id])
 
-    return start_string + ''.join(text_generated)
+    return start_str + ''.join(text_generated)
 
 
-inp = input("Type a starting string: ")
+inp = input("Please type a starting string: ")
 print(generate_text(model, inp))
